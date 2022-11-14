64ade13d22f8168aca48bf87174a758cf947fd2e
Max Base <maxbasecode@gmail.com>
2022-11-14 14:22:14 +0100

    update example

--- example.go
+++ example.go
@@ -14,12 +14,30 @@ func main() {
 	// Create a list
 	list := List{}
 	list.Create(1)
-	list.Insert(2)
-	list.Insert(3)
-	list.Insert(4)
-	list.Insert(5)
-	list.Insert(6)
-	list.Insert(7)
+
+	// Insert some values at begin
+	list.InsertBegin(2)
+	list.InsertBegin(3)
+	list.InsertBegin(4)
+
+	// Insert some values at end
+	list.InsertEnd(5)
+	list.InsertEnd(6)
+	list.InsertEnd(7)
+
+	// Insert some values after
+	list.InsertAfter(8, 5)
+	list.InsertAfter(9, 6)
+	list.InsertAfter(10, 7)
+
+	// Search in the list
+	fmt.Println(list.Search(3))
+
+	// Print the list
+	list.Print()
+
+	// Re-create a list
+	list.Create(100)
 
 	// Print the list
 	list.Print()
@@ -31,4 +49,7 @@ func main() {
 	list.Delete()
 	list.Delete()
 	list.Delete()
+
+	// Print the list
+	list.Print()
 }
